8388c932cb4b31454c9d7ad3103a34ac23b93586
HardCodersReview <120503901+HardCodersReview@users.noreply.github.com>
2022-12-13 21:55:27 +0100

    Add few feature

--- AppToCodeReview1/Controllers/UserWalletController.cs
+++ AppToCodeReview1/Controllers/UserWalletController.cs
@@ -21,31 +21,51 @@ namespace AppToCodeReview1.Controllers
             }
             return totalCredit;
         }
-        public string GetName(int UserId)
+        [HttpPost(Name ="Add User")]
+        public void SetUser([FromBody] User user)
+        {
+            var repo = new UserRepository(new UserWalletContext());
+            repo.AddUser(user);
+
+        }
+        [HttpPost(Name = "Add Card")]
+        public void AddCard([FromBody] Card card)
+        {
+            var repo = new UserRepository(new UserWalletContext());
+            repo.AddCard(card);
+
+        }
+        [HttpPost]
+        public string GetName([FromQuery] int UserId)
         {
             var db = new UserWalletContext();
             var user = db.Users.Single(p => p.UserId == UserId);
 
             return user.UserName;
         }
+        [HttpGet]
         public ActionResult<User> User(int UserId)
         {
             var db = new UserWalletContext();
             return db.Users.Single(p=>p.UserId == UserId);
         }
-        public string GetMonney(int UserId, decimal value)
+        [HttpGet]
+        public string GetMonney([FromBody] int UserId, decimal value)
         {
             var db = new UserWalletContext();
             var user = db.Users.Single(p=>p.UserId == UserId);
-            string avaibleCards = "";
+            string availableCards = "";
             foreach (var item in user.Wallet.Cards)
             {
                 if (item.Credits >= (double)value)
                 {
-                    avaibleCards += item.CardNumber;
+                    availableCards += item.CardNumber+", ";
                 }
             }
-            return avaibleCards;
+            if (availableCards.Length> 0) {
+                availableCards.Remove(availableCards.Length-2);
+            }
+            return availableCards;
 
         }
     }
---
+++ Repository/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class UserRepository
+    {
+        UserWalletContext context;
+        public UserRepository(UserWalletContext context) {
+           
+            this.context = context;
+        }
+        public void AddUser(User user)
+        {
+            try
+            {
+                context.Add(user);
+
+            }catch(Exception ex)
+            {
+
+            }
+         }
+        public void AddCard(int userId, Card card)
+        {
+            var user = context.Users.Single(p=>p.UserId == userId);
+            if (user.Wallet != null)
+            {
+                user.Wallet.Cards.Add(card);
+            }
+            else
+            {
+                user.Wallet = new Wallet();
+                user.Wallet.Cards.Add(card);
+            }
+
+        }
+    }
+}
